8ce8c3ca47c1f5327074ffd60e26a840881f980f
ZARIFMAC <uzbzarif@gmail.com>
2023-03-07 10:36:23 +0500

    add phone

--- api/views.py
+++ api/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse,HttpRequest
+# Import the Smartphone model
+from .models import Smartphone
+import json
 # Create your views here.
 def add_phone(request:HttpRequest):
+    if request.method=='POST':
+        r = request.body
+        data = r.decode()
+        # Convert the data to a dictionary
+        data = json.loads(data)
+        price = data['price'] 
+        img_url = data['img_url']
+        color = data['color']
+        ram = data['ram']
+        memory = data['memory']
+        name = data['name']
+        model = data['model']
+        # Create a new smartphone object
+        phone = Smartphone()
+        phone.price = price
+        phone.save()
+        
+        
+
+        return JsonResponse({'result':data})
     return JsonResponse({})
