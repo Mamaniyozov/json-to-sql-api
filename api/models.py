8c10294c40a3e4693da98103b4cc2e4d7f9920e2
ZARIFMAC <uzbzarif@gmail.com>
2023-03-07 10:41:40 +0500

    default

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Smartphone(models.Model):
     price = models.CharField(max_length=20)
-    img_url = models.CharField(max_length=255)
+    img_url = models.CharField(max_length=255,default='image')
     color = models.CharField(max_length=20)
     ram = models.CharField(max_length=20)
     memory = models.CharField(max_length=20)
