30443c950944e9f238aac4d0c787798cfe13f60a
ZARIFMAC <uzbzarif@gmail.com>
2023-03-07 10:15:52 +0500

    add Smarphone table

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Smartphone(models.Model):
+    price = models.CharField(max_length=20)
+    img_url = models.CharField(max_length=255)
+    color = models.CharField(max_length=20)
+    ram = models.CharField(max_length=20)
+    memory = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
